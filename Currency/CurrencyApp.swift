8088474a7edf452265a58ad774757abdcef95ef5
Başak Ertuğrul <basakertugrul@ceiba-healthcare.com>
2023-06-08 18:21:15 +0300

    Initial Commit

---
+++ Currency/CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyApp.swift
+//  Currency
+//
+//  Created by Başak Ertuğrul on 8.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
